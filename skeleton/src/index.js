e2719e37fadaf320dd095da949d0864a62363fa8
Sydney <sydneyparsonsyb@gmail.com>
2021-08-10 14:56:43 -0400

    step one

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');
+const Game = require('./game.js')
+
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  
+  new Game();
 });
